8ed0b99a2f8561136b0c93c2bfdef15868f698b8
Sahil Gulati <sahilgulati102@gmail.com>
2021-08-20 12:08:41 +0530

    Update index.js

--- node-app/index.js
+++ node-app/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const redis = require('redis');
+//const redis = require('redis');
 
 const app = express();
-const client = redis.createClient({
-  host: 'redis-server',
-  port: 6379,
-});
-client.set('visits', 0);
+//const client = redis.createClient({
+//  host: 'redis-server',
+//  port: 6379,
+//});
+//client.set('visits', 0);
 
 app.get('/', (req, res) => {
-  client.get('visits', (err, visits) => {
-    res.send('Number of visits ' + visits);
-    client.set('visits', parseInt(visits) + 1);
+  //client.get('visits', (err, visits) => {
+   res.send('Number of visits ' + visits);
+   // client.set('visits', parseInt(visits) + 1);
   });
 });
 
